af2843e2c802683c054f407c38eb659ea34c8bef
Kayleigh <57508868+gordokay@users.noreply.github.com>
2023-02-22 09:38:50 -0500

    Implement color block creation

--- app.js
+++ app.js
@@ -1,2 +1,45 @@
+const form = document.querySelector('form');
+const textArea = document.getElementById('hex-codes');
+
+const colorsSection = document.querySelector('section');
+const colorsContainer = document.getElementById('colors');
+const editButton = document.querySelector('button[type="button"]');
+
 const regex = /#?[a-zA-Z0-9]{6}/g;
 
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  showSection();
+  generateColors();
+})
+
+editButton.addEventListener('click', showForm);
+
+function generateColors() {
+  const colors = textArea.value.match(regex);
+  if(!colors) {
+    colorsContainer.textContent = 'Invalid hex codes';
+    return;
+  }
+  colors.map(color => {
+    const colorContainer = document.createElement('div');
+    const colorLabel = document.createElement('p');
+    colorLabel.classList.add('color-label');
+    colorLabel.textContent = color.charAt(0) === '#' ? color : '#' + color;
+    const colorBlock = document.createElement('div');
+    colorBlock.classList.add('color-block');
+    colorBlock.style.backgroundColor = color.charAt(0) === '#' ? color : '#' + color;
+    colorContainer.append(colorLabel, colorBlock);
+    colorsContainer.appendChild(colorContainer);
+  })
+}
+
+function showForm() {
+  colorsSection.classList.add('hidden');
+  form.classList.remove('hidden');
+}
+
+function showSection() {
+  colorsSection.classList.remove('hidden');
+  form.classList.add('hidden');
+}
